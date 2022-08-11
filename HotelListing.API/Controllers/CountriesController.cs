8a028b29ad4f03d0ad8ee1cc22f51790a1e3be9c
Carlos M. Perez <carlosm.perez@gmail.com>
2022-08-11 20:01:24 +0200

    New scaffolded controller and Postman tests.

---
+++ HotelListing.API/Controllers/CountriesController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using HotelListing.API.Data;
+
+namespace HotelListing.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CountriesController : ControllerBase
+{
+    private readonly HotelListingDbContext ctx;
+
+    public CountriesController(HotelListingDbContext context)
+    {
+        ctx = context;
+    }
+
+    // GET: api/Countries
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Country>>> GetCountries()
+    {
+        var countries = await ctx.Countries.ToListAsync();
+        return Ok(countries);
+    }
+
+    // GET: api/Countries/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Country>> GetCountry(int id)
+    {
+        var country = await ctx.Countries.FindAsync(id);
+
+        if (country == null) return NotFound();
+        
+        return Ok(country);
+    }
+
+    // PUT: api/Countries/5
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutCountry(int id, Country country)
+    {
+        if (id != country.Id) return BadRequest("Invalid Record Id");
+
+        ctx.Entry(country).State = EntityState.Modified;
+
+        try
+        {
+            await ctx.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!CountryExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Countries
+    // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+    [HttpPost]
+    public async Task<ActionResult<Country>> PostCountry(Country country)
+    {
+        ctx.Countries.Add(country);
+        await ctx.SaveChangesAsync();
+
+        return CreatedAtAction("GetCountry", new { id = country.Id }, country);
+    }
+
+    // DELETE: api/Countries/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteCountry(int id)
+    {
+        var country = await ctx.Countries.FindAsync(id);
+
+        if (country == null) return NotFound();
+
+        ctx.Countries.Remove(country);
+        await ctx.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool CountryExists(int id)
+    {
+        return ctx.Countries.Any(e => e.Id == id);
+    }
+}
